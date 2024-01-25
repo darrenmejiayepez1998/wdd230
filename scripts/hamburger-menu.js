d8761d8ed209c3536d9e1bf9819c80ec30b50839
darren.mejia.yepez1998 <darren.mejia.yepez@gmail.com>
2024-01-25 14:53:12 -0700

    team act

---
+++ scripts/hamburger-menu.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('nav');
+
+    hamButton.addEventListener('click', () => {
+        hamButton.classList.toggle('open');
+        navigation.classList.toggle('open');
+    });
+});
