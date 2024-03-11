377e471287f6d4c6a5f41777428c5c788ff2867c
darren.mejia.yepez1998 <darren.mejia.yepez@gmail.com>
2024-03-11 14:55:54 -0600

    example of fetching JSON data API

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,36 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        // builds the h2 content
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        // builds the image information
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Protrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        // adds the card one at a time
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+   // console.table(data.prophets);
+   displayProphets(data.prophets);
+}
+
+getProphetData();
+
