e72b9d05800f368a82dcec5a00ec044d138cbaed
darren.mejia.yepez1998 <darren.mejia.yepez@gmail.com>
2024-03-15 14:02:34 -0600

    uodated weatrhre

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64appid=cac9f7c8276d2ae47cd3f21893b9d470';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=cac9f7c8276d2ae47cd3f21893b9d470&units=imperial';
 
 async function apiFetch() {
     try {
@@ -10,12 +10,22 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data) // for testing only
+            displayResults(data)
         } else {
             throw Error(await response.text());
         }
     } catch (error) {
         console.log(error);
     }
+}
+
+function displayResults(data) {
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+    currentTemp.textContent = `${data.main.temp} Degrees F`;
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+    captionDesc.textContent = data.weather[0].description; 
 }
 
 apiFetch();
