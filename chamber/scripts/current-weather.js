bfd9557e26cbc1625590b916c52f8e74c62a9322
darren.mejia.yepez1998 <darren.mejia.yepez@gmail.com>
2024-03-22 15:28:46 -0600

    updated chamber site

--- chamber/scripts/current-weather.js
+++ chamber/scripts/current-weather.js
@@ -21,36 +21,35 @@ function displayForecast(data) {
     const forecastContainer = document.getElementById('forecast-container');
     forecastContainer.innerHTML = ''; // clears previous forecast data
 
-     data.list.forEach(forecast => {
-        const dateTime = new Date(forecast.dt + 1000);
-        const temperature = Math.round(forecast.temp.day);
-        const weatherDescription = forecast.weather[0].description;
-        const iconCode = forecast.weather[0].icon;
-        const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+    data.list.forEach(forecast => {
+    const dateTime = new Date(forecast.dt + 1000);
+    const temperature = Math.round(forecast.temp.day);
+    const weatherDescription = forecast.weather[0].description;
+    const iconCode = forecast.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-        const forecastElement = document.createElement('div');
-        forecastElement.classList.add('forecast-day');
+    const forecastElement = document.createElement('div');
+    forecastElement.classList.add('forecast-day');
 
-        const dateElement = document.createElement('p');
-        dateElement.textContent = dateTime.toDateString();
+    const dateElement = document.createElement('p');
+    dateElement.textContent = dateTime.toDateString();
 
-        const tempElement = document.createElement('p');
-        tempElement.textContent = `Temperature: ${temperature} °C`;
+    const tempElement = document.createElement('p');
+    tempElement.textContent = `Temperature: ${temperature} °C`;
 
-        const descElement = document.createElement('p');
-        descElement.textContent = `Condition: ${weatherDescription}`;
+    const descElement = document.createElement('p');
+    descElement.textContent = `Condition: ${weatherDescription}`;
 
-        const iconElement = document.createElement('img');
-        iconElement.src = iconUrl;
-        iconElement.alt = weatherDescription;
+    const iconElement = document.createElement('img');
+    iconElement.src = iconUrl;
+    iconElement.alt = weatherDescription;
 
-        forecastElement.appendChild(dateElement);
-        forecastElement.appendChild(tempElement);
-        forecastElement.appendChild(descElement);
-        forecastElement.appendChild(iconElement);
+    forecastElement.appendChild(dateElement);
+    forecastElement.appendChild(tempElement);
+    forecastElement.appendChild(descElement);
+    forecastElement.appendChild(iconElement);
 
-        forecastContainer.appendChild(forecastElement);
+    forecastContainer.appendChild(forecastElement);
     });
 }
-
 document.addEventListener('DOMContentLoaded', fetchWeatherData);
