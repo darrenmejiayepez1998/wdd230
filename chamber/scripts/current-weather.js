116946a919e4745576dad6c04a45fafae5a749ea
darren.mejia.yepez1998 <darren.mejia.yepez@gmail.com>
2024-03-22 13:50:49 -0600

    updated hero image

--- chamber/scripts/current-weather.js
+++ chamber/scripts/current-weather.js
@@ -1,7 +1,8 @@
 async function fetchWeatherData() {
     const apiKey = "cac9f7c8276d2ae47cd3f21893b9d470"
-    const city = "Orem";
-    const forecasturl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    const latitude = "40.30";
+    const longitude = "-111.70";
+    const forecasturl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
 
     try {
         const response = await fetch(forecasturl);
@@ -12,7 +13,7 @@ async function fetchWeatherData() {
             throw new Error('Failed to fetch forecast data');
         }
     } catch (error) {
-        console.error('Error fetching forecast data');
+        console.error('Error fetching forecast data: ', error);
     }
 }
 
@@ -20,17 +21,18 @@ function displayForecast(data) {
     const forecastContainer = document.getElementById('forecast-container');
     forecastContainer.innerHTML = ''; // clears previous forecast data
 
-    for (let i=0; i < 3; i++) {
-        const forecast = data.list[i * 8];
-        const date = new Date(forecast.dt + 1000);
+     data.list.forEach(forecast => {
+        const dateTime = new Date(forecast.dt + 1000);
+        const temperature = Math.round(forecast.temp.day);
         const weatherDescription = forecast.weather[0].description;
+        const iconCode = forecast.weather[0].icon;
         const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
 
         const forecastElement = document.createElement('div');
         forecastElement.classList.add('forecast-day');
 
         const dateElement = document.createElement('p');
-        dateElement.textContent = date.toDateString();
+        dateElement.textContent = dateTime.toDateString();
 
         const tempElement = document.createElement('p');
         tempElement.textContent = `Temperature: ${temperature} °C`;
@@ -48,7 +50,7 @@ function displayForecast(data) {
         forecastElement.appendChild(iconElement);
 
         forecastContainer.appendChild(forecastElement);
-    }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', fetchWeatherData);
